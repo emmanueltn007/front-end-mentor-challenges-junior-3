b6061b5289dd484c078da43b44fa4568afc7db82
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-04-30 23:46:43 +0200

    added new folder product-list-with-cart from front-end-mentor-challenges-junior-2

---
+++ product-list-with-cart/src/App.jsx
@@ -0,0 +1,11 @@
+import Container from "./components/Container";
+
+function App () {
+  return (
+    <div className="font-['Red Hat Text',sans-serif] bg-[hsl(13,31%,94%)]">
+      <Container />
+    </div>
+  );
+}
+
+export default App
